701539554ee9686680aeafd87e3ca9c6b48c4945
James Guana <jdpguana@histark.com>
2025-01-17 11:31:17 +0800

    Update test.py

--- test.py
+++ test.py
@@ -13,7 +13,10 @@ def get_submodule_paths():
 submodule_paths = get_submodule_paths()
 
 for module_dir in submodule_paths:
+    os.chdir(module_dir)
     print(f"Entering {module_dir}")
-    if os.path.isfile(os.path.join(module_dir, 'requirements.txt')):
-        subprocess.run(['pip', 'install', '-r', os.path.join(module_dir, 'requirements.txt')], check=True)
-    subprocess.run(['python', os.path.join(module_dir, 'test.py')], check=True)
+    if os.path.isfile('requirements.txt'):
+        subprocess.run(['pip', 'install', '-r', 'requirements.txt'], check=True)
+    subprocess.run(['python', 'test.py'], check=True)
+    os.chdir('..')
+    print(f"Exiting {module_dir}")
