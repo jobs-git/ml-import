a3c014c840e592e38eee078c6ad7a079b764df55
James Guana <jdpguana@histark.com>
2025-01-21 11:12:35 +0800

    Make conflict checks more descriptive

--- test_conflicts.py
+++ test_conflicts.py
@@ -1,28 +1,38 @@
 import os
 import ast
 
+class FunctionConflictVisitor(ast.NodeVisitor):
+    def __init__(self):
+        self.functions = {}
+        self.conflicts = []
+
+    def visit_FunctionDef(self, node):
+        if node.name in self.functions:
+            self.conflicts.append((node.name, self.functions[node.name], (node.lineno, node.col_offset)))
+        else:
+            self.functions[node.name] = (node.lineno, node.col_offset)
+        self.generic_visit(node)
+
 def find_conflicts(directory):
-    functions = set()
-    conflicts = set()
+    conflicts = []
     
     for root, _, files in os.walk(directory):
         for file in files:
             if file.endswith('.py'):
                 with open(os.path.join(root, file), 'r', encoding='utf-8') as f:
                     tree = ast.parse(f.read(), filename=file)
-                    for node in ast.walk(tree):
-                        if isinstance(node, ast.FunctionDef):
-                            if node.name in functions:
-                                conflicts.add(node.name)
-                            else:
-                                functions.add(node.name)
+                    visitor = FunctionConflictVisitor()
+                    visitor.visit(tree)
+                    if visitor.conflicts:
+                        conflicts.extend([(file, conflict[0], conflict[1], conflict[2]) for conflict in visitor.conflicts])
     
     return conflicts
 
 if __name__ == "__main__":
     conflicts = find_conflicts('.')
     if conflicts:
-        print(f"Conflicting functions found: {', '.join(conflicts)}")
+        for file, func_name, (line1, col1), (line2, col2) in conflicts:
+            print(f"Conflict found in {file}:\n  Function '{func_name}' at line {line1}, column {col1}\n  Conflict with definition at line {line2}, column {col2}")
         exit(1)
     else:
         print("No conflicts found.")
