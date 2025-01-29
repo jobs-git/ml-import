47512e0cf13d8cae1df98f9c724c72e27803f060
James Guana <jdpguana@histark.com>
2025-01-29 23:24:44 +0800

    Update code formatting

--- test.py
+++ test.py
@@ -1,23 +1,23 @@
 import os
 import subprocess
 
-def get_submodule_paths():
+def get_submodule_paths ():
     submodule_paths = []
-    with open('.gitmodules') as file:
+    with open ('.gitmodules') as file:
         for line in file:
             if 'path =' in line:
-                path = line.strip().split('= ')[1]
-                submodule_paths.append(path)
+                path = line.strip ().split ('= ')[1]
+                submodule_paths.append (path)
     return submodule_paths
 
-submodule_paths = get_submodule_paths()
+submodule_paths = get_submodule_paths ()
 
 # initiate test runs
 for module_dir in submodule_paths:
-    os.chdir(module_dir)
-    print(f"Entering {module_dir}")
-    if os.path.isfile('requirements.txt'):
-        subprocess.run(['pip', 'install', '-r', 'requirements.txt'], check=True)
-    subprocess.run(['python', '-u', 'test.py'], check=True)
-    os.chdir('..')
-    print(f"Exiting {module_dir}")
+    os.chdir (module_dir)
+    print (f"Entering {module_dir}")
+    if os.path.isfile ('requirements.txt'):
+        subprocess.run (['pip', 'install', '-r', 'requirements.txt'], check = True)
+    subprocess.run (['python', '-u', 'test.py'], check = True)
+    os.chdir ('..')
+    print (f"Exiting {module_dir}")
